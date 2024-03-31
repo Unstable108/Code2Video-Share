cc29093febbe3d782d757e3a1315f0212019f5eb
Debasish Das <68758489+Unstable108@users.noreply.github.com>
2024-03-31 19:58:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
 const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const http = require("http");
+const socketIO = require("socket.io");
 const { v4: uuidV4 } = require("uuid");
-const bp = require("body-parser");
+const bodyParser = require("body-parser");
 const url = require("url");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
-app.use(bp.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-var arr = {};
+const connectedUsers = {};
 
-io.on("connection", function (socket) {
-  socket.on("join", function (uid, id, name) {
-    console.log(id);
+io.on("connection", (socket) => {
+  socket.on("join", (uid, id, name) => {
+    console.log(`${id} joined the room ${uid}`);
     socket.join(uid);
-    if (arr.hasOwnProperty(uid)) {
-      arr[uid].push(name);
-    } else {
-      arr[uid] = [name];
-    }
-
+    
+    connectedUsers[uid] = connectedUsers[uid] || [];
+    connectedUsers[uid].push(name);
+    
     socket.broadcast.to(uid).emit("user-connected", id);
-    io.sockets.to(uid).emit("name", arr[uid]);
-    console.log(arr[uid]);
-    socket.on("disconnect", function () {
-      socket.broadcast.to(uid).emit("user-dis", id);
+    io.sockets.to(uid).emit("name", connectedUsers[uid]);
+    
+    socket.on("disconnect", () => {
+      console.log(`${id} left the room ${uid}`);
+      const index = connectedUsers[uid].indexOf(name);
+      if (index !== -1) {
+        connectedUsers[uid].splice(index, 1);
+        socket.broadcast.to(uid).emit("user-dis", id);
+      }
     });
   });
 
-  socket.on("message", function (name, message, uid) {
-    console.log(name);
-    console.log(message);
+  socket.on("message", (name, message, uid) => {
+    console.log(`${name}: ${message}`);
     socket.broadcast.to(uid).emit("receive", name, message);
   });
 
-  socket.on("editor_message", function (message, uid) {
+  socket.on("editor_message", (message, uid) => {
     socket.broadcast.to(uid).emit("text_editor", message);
   });
 });
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/join", function (req, res) {
+app.get("/join", (req, res) => {
   res.redirect(
     url.format({
       pathname: `/${uuidV4()}`,
@@ -56,7 +61,7 @@ app.get("/join", function (req, res) {
   );
 });
 
-app.get("/joinold", function (req, res) {
+app.get("/joinold", (req, res) => {
   res.redirect(
     url.format({
       pathname: req.query.meeting_id,
@@ -65,13 +70,13 @@ app.get("/joinold", function (req, res) {
   );
 });
 
-app.get("/:id", function (req, res) {
+app.get("/:id", (req, res) => {
   const uid = req.params.id;
   console.log(uid);
-  const nam = req.query.name;
-  res.render("file", { id: uid, name: nam });
+  const name = req.query.name;
+  res.render("file", { id: uid, name: name });
 });
 
-server.listen(port, function () {
+server.listen(port, () => {
   console.log(`App is listening at port ${port}`);
 });
